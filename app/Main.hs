04d69141633108aabb997fdf78a1f03adf0d1790
Philipp Krüger <matheus.dev@gmail.com>
2017-06-09 09:05:05 +0200

    Working prototype of displaying a record that has focusable parts. Model is encoded as Zipper.

--- app/Main.hs
+++ app/Main.hs
@@ -23,10 +23,22 @@ import qualified Widgets.Record as Record
 import Widgets.Record (RecordModelFocused(..), RecordFieldModelFocused(..))
 import Widgets.Activatable (ActiveOr(..))
 import qualified Widgets.TextField as TextField
+import Widgets.RecordType as RT
 
 main :: IO ()
-main = runReactive (move (V2 100 100) . alignHV (0, 0) . Record.viewFocused)
+main = runReactive (move (V2 100 100) . alignHV (0, 0) . RT.view)
+  (RT.TypeModel RT.NumberType
+    (RT.InRecordType
+      (RT.InRecordType RT.Top [] "pos" [("name", RT.TypeHole)])
+      [("x", RT.NumberType)] "y" []))
+{-main = runReactive (move (V2 100 100) . alignHV (0, 0) . Record.viewFocused)
   (RecordFocused [] (RecordFieldFocused (TextField.construct "Field1" "")) [])
-{-main = runReactive (move (V2 100 100) . alignHV (0, 0) . Expression.view)
+main = runReactive (move (V2 100 100) . alignHV (0, 0) . Expression.view)
   (ValueHole (TypeHole Nothing) Nothing)
 -}
+
+example =
+  (RT.TypeModel RT.NumberType
+    (RT.InRecordType
+      (RT.InRecordType RT.Top [] "pos" [("name", RT.TypeHole)])
+      [("x", RT.NumberType)] "y" []))
